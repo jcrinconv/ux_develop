ccda19e47402923990f98e6f2730f2c985e9800b
vanessaZunigaL <b.zuniga@uniandes.edu.co>
2026-09-18 21:39:40 -0500

    se agrega configuracion base y se crea primer componente

---
+++ web_recetapp/src/app/app.routes.ts
@@ -0,0 +1,11 @@
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () =>
+      import('./dashboard/dashboard.component').then((m) => m.DashboardComponent),
+  },
+  { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+  { path: '**', redirectTo: 'dashboard' },
+];
